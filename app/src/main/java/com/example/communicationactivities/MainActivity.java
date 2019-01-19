740cad2c829d4245728c20543bde65ca6bf27355
azarrias <adolfo.zarrias@gmail.com>
2019-01-19 20:09:43 +0100

    Retrieve data in the secondary activity

--- app/src/main/java/com/example/communicationactivities/MainActivity.java
+++ app/src/main/java/com/example/communicationactivities/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.communicationactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnVerify;
+    private EditText etName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.et_name);
+        btnVerify = findViewById(R.id.btn_verify);
+
+        btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), SecondaryActivity.class);
+                i.putExtra("data_name", etName.getText().toString());
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/communicationactivities/SecondaryActivity.java
+++ app/src/main/java/com/example/communicationactivities/SecondaryActivity.java
@@ -2,12 +2,27 @@ package com.example.communicationactivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondaryActivity extends AppCompatActivity {
+
+    TextView tv_greeting;
+    String tvGreetingStr1;
+    String tvGreetingStr2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+
+        Bundle extras = getIntent().getExtras();
+
+        tv_greeting = findViewById(R.id.tv_greeting);
+        tvGreetingStr1 = getString(R.string.tv_greeting_str1);
+        tvGreetingStr2 = getString(R.string.tv_greeting_str2);
+
+        String greeting = tvGreetingStr1 + " " + extras.getString("data_name") + tvGreetingStr2;
+
+        tv_greeting.setText(greeting);
     }
 }
