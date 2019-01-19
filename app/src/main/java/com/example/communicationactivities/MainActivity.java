5da4273ce7ccb94f6dcd339850a3b1540b8b453d
azarrias <adolfo.zarrias@gmail.com>
2019-01-19 20:35:09 +0100

    Circle back the result of the secondary activity to the main activity

--- app/src/main/java/com/example/communicationactivities/MainActivity.java
+++ app/src/main/java/com/example/communicationactivities/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.communicationactivities;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnVerify;
     private EditText etName;
+    private static final int reqCode = 1;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +23,25 @@ public class MainActivity extends AppCompatActivity {
 
         etName = findViewById(R.id.et_name);
         btnVerify = findViewById(R.id.btn_verify);
+        tvResult = findViewById(R.id.tv_result);
 
         btnVerify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(v.getContext(), SecondaryActivity.class);
                 i.putExtra("data_name", etName.getText().toString());
-                startActivity(i);
+                startActivityForResult(i, reqCode);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == reqCode && resultCode == RESULT_OK) {
+            String result = getString(R.string.tv_result_str) + " "
+                    + data.getExtras().getString("data_result");
+
+            tvResult.setText(result);
+        }
     }
 }
--- app/src/main/java/com/example/communicationactivities/SecondaryActivity.java
+++ app/src/main/java/com/example/communicationactivities/SecondaryActivity.java
@@ -1,7 +1,10 @@
 package com.example.communicationactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondaryActivity extends AppCompatActivity {
@@ -9,6 +12,8 @@ public class SecondaryActivity extends AppCompatActivity {
     TextView tv_greeting;
     String tvGreetingStr1;
     String tvGreetingStr2;
+    Button btnAgree;
+    Button btnRefuse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +23,34 @@ public class SecondaryActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
 
         tv_greeting = findViewById(R.id.tv_greeting);
+        btnAgree = findViewById(R.id.btn_agree);
+        btnRefuse = findViewById(R.id.btn_refuse);
         tvGreetingStr1 = getString(R.string.tv_greeting_str1);
         tvGreetingStr2 = getString(R.string.tv_greeting_str2);
 
         String greeting = tvGreetingStr1 + " " + extras.getString("data_name") + tvGreetingStr2;
 
         tv_greeting.setText(greeting);
+
+        btnAgree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendResult("Agree");
+            }
+        });
+
+        btnRefuse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendResult("Refuse");
+            }
+        });
+    }
+
+    private void sendResult(String value) {
+        Intent i = new Intent();
+        i.putExtra("data_result", value);
+        setResult(RESULT_OK, i);
+        finish();
     }
 }
